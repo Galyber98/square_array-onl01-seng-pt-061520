94d2a1eec0630e998dfa9b9329b0f45a06a0bef2
Galilea Bernal <galilea.bernal98@gmail.com>
2020-06-26 01:13:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,7 @@
 numbers = [1, 2, 3]
 
 def square_array(array)
-  array.each do |number|
+  array.collect do |number|
   puts array
   end
 end
-
-def new_numbers(array)
-  array.collect do |number|
-    puts array
-  end
-end 
-  
